e103f6608767f566f53daca84ff14bb3c0e9a975
yuchen <dev.chenyu@gmail.com>
2021-08-07 23:56:56 +0800

    实现了部分跳转逻辑

--- app/src/main/java/com/example/bcicare/StartActivity.java
+++ app/src/main/java/com/example/bcicare/StartActivity.java
@@ -2,13 +2,31 @@ package com.example.bcicare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
 
+    private static final String TAG = "StartActivity";
+
+    TextView tv_toStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        tv_toStart = findViewById(R.id.tv_toStart);
+        tv_toStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+                StartActivity.this.finish();
+            }
+        });
+
     }
 }
