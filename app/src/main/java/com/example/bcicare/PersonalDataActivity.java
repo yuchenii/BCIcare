0a485fd4241bf79e1b1b7f6ad91576ef6b0ebecc
yuchen <dev.chenyu@gmail.com>
2021-08-13 23:27:16 +0800

    个人资料界面做了一部分

---
+++ app/src/main/java/com/example/bcicare/PersonalDataActivity.java
@@ -0,0 +1,14 @@
+package com.example.bcicare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_data);
+    }
+}
