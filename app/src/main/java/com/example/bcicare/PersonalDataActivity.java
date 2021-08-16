522f7c83e5e2712761221073a345e34e7d2e31fc
yuchen <dev.chenyu@gmail.com>
2021-08-16 23:14:35 +0800

    修改了String.xml

--- app/src/main/java/com/example/bcicare/PersonalDataActivity.java
+++ app/src/main/java/com/example/bcicare/PersonalDataActivity.java
@@ -5,25 +5,41 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class PersonalDataActivity extends AppCompatActivity {
+
+    private static final String TAG = "PersonalDataActivity";
+
+    TextView tv_change_avatars;
+    TextView tv_real_name;
+    TextView tv_gender;
+    TextView tv_patient_type;
+    TextView tv_attending_doctor;
+    TextView tv_medicine_method;
+    TextView tv_device_ip;
+    TextView tv_guardian_mode;
+    TextView tv_guardian_name;
+    TextView tv_guardian_phone;
+    TextView tv_logout;
+    Switch switch_guardian_mode;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_data);
 
-        initToolbar("个人资料");
+        initToolbar();
     }
 
     /**
      * 初始化 toolbar
-     *
-     * @param title toolbar标题
      */
-    protected void initToolbar(String title) {
+    protected void initToolbar() {
 
+        String title = "个人资料";
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         // 设置title
